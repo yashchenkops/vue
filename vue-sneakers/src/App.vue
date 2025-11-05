570404d6352bfbac04fefe18e37d94790634aafa
Pavel <yashchenkodev@gmail.com>
2025-11-05 11:05:30 +0200

    initial commit

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import MyHello from './components/My-Hello.vue';
+import MyHello from './components/MyHello.vue'
 </script>
 
 <template>
+  <h1>Hello world!</h1>
+  <h2>React, Vue, Angular</h2>
+
   <MyHello />
 </template>
---
+++ vue-sneakers/src/components/MyHello.vue
@@ -0,0 +1,15 @@
+<script setup>
+alert('lol')
+</script>
+
+
+<template>
+<p class="text">My component</p>
+<button>Open</button>
+</template>
+
+<style scoped>
+.text {
+  background-color: red;
+}
+</style>
