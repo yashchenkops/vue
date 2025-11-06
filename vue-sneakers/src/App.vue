7adbd754de1aaee4f62af3bcfedf923e1d1923ff
Pavel <yashchenkodev@gmail.com>
2025-11-06 19:56:14 +0200

    test directives

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -3,8 +3,5 @@ import MyHello from './components/MyHello.vue'
 </script>
 
 <template>
-  <h1>Hello world!</h1>
-  <h2>React, Vue, Angular</h2>
-
   <MyHello />
 </template>
--- vue-sneakers/src/components/MyHello.vue
+++ vue-sneakers/src/components/MyHello.vue
@@ -1,15 +1,16 @@
 <script setup>
-alert('lol')
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
 </script>
 
-
 <template>
-<p class="text">My component</p>
-<button>Open</button>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+  <input @keyup.ctrl.enter="increment" />
 </template>
-
-<style scoped>
-.text {
-  background-color: red;
-}
-</style>
