f62e4174601f9675d2bc6c36825a34ac51e6a336
Pavel <yashchenkodev@gmail.com>
2025-11-09 00:14:07 +0200

    feat: add filters

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Header from './components/Header.vue'
-import CardList from './components/CardList.vue';
-import Drawer from './components/Drawer.vue';
+import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
@@ -10,9 +10,23 @@ import Drawer from './components/Drawer.vue';
     <Header />
 
     <div class="p-10">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-        <input type="text">
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none" name="" id="">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">По цене (дорогие)</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
+            <input
+              class="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск"
+            />
+          </div>
+        </div>
       </div>
 
       <CardList />
